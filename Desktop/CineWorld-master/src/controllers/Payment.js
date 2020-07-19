fb3ad22d059dde07530dde492470c7ff3249aa47
MunirAbobaker <46633767+MunirAbobaker@users.noreply.github.com>
2020-07-19 10:42:55 +0200

    delete payment configurations

--- Desktop/CineWorld-master/src/controllers/Payment.js
+++ Desktop/CineWorld-master/src/controllers/Payment.js
@@ -17,8 +17,8 @@ class Payment {
     paypalConfigure() {
         paypal.configure({
             'mode': 'sandbox', //sandbox or live
-            'client_id': 'AcriMC4sfpa8lp6hu9-hVzGfslrywuCwDYQi-inJeDoK_E51YtOOpRHx0CX9HNrpu_OcKSrQVYplbEuA',
-            'client_secret': 'EPA_Gmn7E3XCe-V7_z1Qs29EdqZDahZ9rb7AaMyQalFBP9RIbvs5KY4IKITNIzfOqMTAy5kIp7cAVD5G'
+            'client_id': '',
+            'client_secret': ''
         });
     }
 
